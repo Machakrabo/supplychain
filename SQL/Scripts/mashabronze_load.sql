ec7c6ca9bc813a717591f5daa20f2e96006aaeae
Maloshree C <maloshree.mukherjee@gmail.com>
2025-09-17 13:48:20 +0200

    Create mashabronze_load.sql

---
+++ SQL/Scripts/mashabronze_load.sql
@@ -0,0 +1,63 @@
+/*
+=========================================
+Data Load in MashaBronze Layer
+=========================================
+
+Purpose: This SQL script performs a bulk data import into the mashabronze schema within the MashaDB database. Specifically, it loads structured data from CSV files into key tables: currency, calendar, locations, product, and actuals_qty.
+Assumes the first row of each CSV file is a header and skips it. Uses the TABLOCK option to improve performance during insert.Applies specific data formatting options for proper handling of delimiters, row terminators, and encoding (e.g., UTF-8 via CODEPAGE = '65001').
+Executes a full load for the actuals_qty table by truncating existing data before importing fresh data. In bronze layer we will just load the original data.
+
+Warning : 
+The TRUNCATE TABLE mashabronze.actuals_qty command permanently deletes all existing data in the actuals_qty table before loading new data. 
+Ensure this is intentional, and backups exist if needed.
+*/
+USE MashaDB;
+BULK INSERT mashabronze.currency
+FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Currency.csv'
+WITH(
+FIRSTROW= 2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+GO
+BULK INSERT mashabronze.calendar
+FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Calender.csv'
+WITH(
+FIRSTROW= 2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+GO
+BULK INSERT mashabronze.locations
+FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Location.csv'
+WITH(
+    FIRSTROW = 2,              -- Skip the header row
+    FIELDTERMINATOR = ',',     -- Columns are separated by commas
+    ROWTERMINATOR = '\n',      -- Lines end with a line break
+    CODEPAGE = '65001',        --  to support special characters
+    FORMAT = 'CSV',            -- Tells SQL what type of file is it
+    TABLOCK  
+);
+GO
+BULK INSERT mashabronze.product
+FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Products.csv'
+WITH(
+FIRSTROW= 2,
+FIELDTERMINATOR=',',
+ROWTERMINATOR = '\n', 
+TABLOCK 
+);
+GO
+--- Full Load
+TRUNCATE TABLE mashabronze.actuals_qty
+BULK INSERT mashabronze.actuals_qty
+FROM
+'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Actuals_qty.csv'
+WITH(
+FIRSTROW= 2,
+FIELDTERMINATOR=',',
+    ROWTERMINATOR = '\n',
+    CODEPAGE = '65001',
+    FORMAT = 'CSV',
+TABLOCK 
+);
