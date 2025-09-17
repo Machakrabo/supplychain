eae32b26a995e4616687f28579825d704198cae6
Maloshree C <maloshree.mukherjee@gmail.com>
2025-09-17 17:45:42 +0200

    Update mashabronze_load.sql

--- SQL/Scripts/mashabronze_load.sql
+++ SQL/Scripts/mashabronze_load.sql
@@ -13,6 +13,12 @@ Ensure this is intentional, and backups exist if needed.
 */
 CREATE OR ALTER PROCEDURE mashabronze.load_mashabronze AS
 BEGIN
+PRINT'=======================================';
+PRINT'>>>>> Loading the BronzeLayer <<<<<';
+PRINT'=======================================';
+PRINT'----------Currency Data Load-----------';
+--- bulk Load
+DELETE FROM mashabronze.currency;
 BULK INSERT mashabronze.currency
 FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Currency.csv'
 WITH(
@@ -20,6 +26,10 @@ FIRSTROW= 2,
 FIELDTERMINATOR=',',
 TABLOCK
 );
+SELECT COUNT(*) FROM mashabronze.currency;
+PRINT'----------Calendar Data Load-----------';
+--- Full Load
+DELETE FROM  mashabronze.calendar
 BULK INSERT mashabronze.calendar
 FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Calender.csv'
 WITH(
@@ -27,6 +37,10 @@ FIRSTROW= 2,
 FIELDTERMINATOR=',',
 TABLOCK
 );
+SELECT COUNT(*) FROM mashabronze.calendar;
+PRINT'----------Location Data Load-----------';
+--- Full Load
+DELETE FROM mashabronze.locations
 BULK INSERT mashabronze.locations
 FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Location.csv'
 WITH(
@@ -37,7 +51,10 @@ WITH(
     FORMAT = 'CSV',            -- Tells SQL what type of file is it
     TABLOCK  
 );
-
+SELECT COUNT(*) FROM mashabronze.locations;
+PRINT'----------Product Data Load-----------';
+--- Full Load
+DELETE FROM  mashabronze.product
 BULK INSERT mashabronze.product
 FROM 'D:\Documents\Maloshree\2025\Project- Supply Chain\Data\csv2\Products.csv'
 WITH(
@@ -46,6 +63,8 @@ FIELDTERMINATOR=',',
 ROWTERMINATOR = '\n', 
 TABLOCK 
 );
+SELECT COUNT(*) FROM mashabronze.product;
+PRINT'----------Actuals Qty Data Load-----------';
 --- Full Load
 TRUNCATE TABLE mashabronze.actuals_qty
 BULK INSERT mashabronze.actuals_qty
@@ -59,4 +78,6 @@ FIELDTERMINATOR=',',
     FORMAT = 'CSV',
 TABLOCK 
 );
+SELECT COUNT(*) FROM mashabronze.actuals_qty;
+PRINT'------------------------------------';
 END
