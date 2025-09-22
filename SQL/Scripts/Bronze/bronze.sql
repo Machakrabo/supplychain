fe7ef4ea8b550fc51871904ca2e12f55129659f2
Maloshree C <maloshree.mukherjee@gmail.com>
2025-09-22 10:16:55 +0200

    Update bronze.sql

--- SQL/Scripts/Bronze/bronze.sql
+++ SQL/Scripts/Bronze/bronze.sql
@@ -19,15 +19,20 @@ Best Practices:
 - Avoid making schema changes directly in production without testing.
 - Use DROP TABLE IF EXISTS if you want to re-create tables safely.
 */
+
 USE MashaDB;
 GO
+IF OBJECT_ID ('mashabronze.locations', 'U')IS NOT NULL
+  DROP TABLE mashabronze.locations;
 CREATE TABLE mashabronze.locations(
 loc_id VARCHAR(50),
-loc_details VARCHAR(50),
+loc_details NVARCHAR(MAX),
 loc_region VARCHAR(50),
 PRIMARY KEY(loc_id)
 );
 GO
+IF OBJECT_ID ('mashabronze.product', 'U')IS NOT NULL
+  DROP TABLE mashabronze.product;
 CREATE TABLE mashabronze.product(
 prd_id VARCHAR(50),
 prd_season VARCHAR(50),
@@ -39,29 +44,36 @@ prd_sku VARCHAR(50),
 PRIMARY KEY(prd_id)
 );
 GO
+IF OBJECT_ID ('mashabronze.currency', 'U')IS NOT NULL
+  DROP TABLE mashabronze.currency;
 CREATE TABLE mashabronze.currency(
 currency_id VARCHAR(50),
 currency_desc VARCHAR(50),
 PRIMARY KEY(currency_id)
 );
---DROP TABLE mashabronze.currency;
+
 GO
+IF OBJECT_ID ('mashabronze.calendar', 'U')IS NOT NULL
+  DROP TABLE mashabronze.calendar;
 CREATE TABLE mashabronze.calendar(
 dates DATE,
 holidays INT
 PRIMARY KEY(dates)
 );
 GO
+IF OBJECT_ID ('mashabronze.actuals_qty', 'U')IS NOT NULL
+  DROP TABLE mashabronze.actuals_qty;
 CREATE TABLE mashabronze.actuals_qty (
    dates DATE,
    loc_id VARCHAR(50),
    prd_id VARCHAR(50),
    prd_price VARCHAR(50),
-   actuals_qty INT,
    currency_id VARCHAR(50),
+   actuals_qty INT,
    PRIMARY KEY(dates, loc_id, prd_id),
-   FOREIGN KEY(loc_id) REFERENCES mashabronze.locations(loc_id),
-   FOREIGN KEY(prd_id) REFERENCES mashabronze.product(prd_id),
-   FOREIGN KEY(dates) REFERENCES mashabronze.calendar(dates),
-   FOREIGN KEY(currency_id) REFERENCES mashabronze.currency(currency_id)
+   --dwh_create_date DATETIME2 DEFAULT GETDATE()
+   --FOREIGN KEY(loc_id) REFERENCES mashabronze.locations(loc_id),
+   --FOREIGN KEY(prd_id) REFERENCES mashabronze.product(prd_id),
+   --FOREIGN KEY(dates) REFERENCES mashabronze.calendar(dates),
+   --FOREIGN KEY(currency_id) REFERENCES mashabronze.currency(currency_id)
 );
